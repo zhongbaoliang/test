cb14a0e8faa38daa1e3603a7de1454f57b2c153f
zhongbaol <2018191080@qq.com>
2021-05-07 20:38:47 +0800

    win

--- src/thread/threadTest.java
+++ src/thread/threadTest.java
@@ -3,7 +3,7 @@ package thread;
 public class threadTest {
     public static void main(String arg[]){
         PrimeThread primeThread = new PrimeThread(10);
-        primeThread.run();
+        primeThread.start();
         for(int i=0;i<10;i++)
             System.out.println("Hello ,i am helloCode " + i);
     }
