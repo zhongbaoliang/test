52710f900e4e8cfdce390fa5cb5109125e94fe3e
zhongbaol <2018191080@qq.com>
2021-05-08 18:36:57 +0800

    5.8

--- src/ThreadTest/ImplementsRunnable.java
+++ src/ThreadTest/ImplementsRunnable.java
@@ -10,16 +10,23 @@ public class ImplementsRunnable implements Runnable {
     @Override
     public void run(){
         for(int i=0;i<data;i++){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Hello, i am ImplementsRunnable sub thread " + i);
         }
     }
 
-    public static void main(String arg[]){
+    public static void main(String arg[]) throws InterruptedException {
         ImplementsRunnable implementsRunnable =new ImplementsRunnable(10);
         Thread thread =new Thread(implementsRunnable);
         thread.start();
 
+
         for(int i=0;i<10;i++){
+            Thread.sleep(100);
             System.out.println("Hello, i am ImplementsRunnable Main thread " + i);
         }
 
--- src/ThreadTest/ThreadTest.java
+++ src/ThreadTest/ThreadTest.java
@@ -1,5 +1,7 @@
 package ThreadTest;
 
+//匿名函数，lambda，线程池
+//两个线程交替执行
 public class ThreadTest {
     public static void main(String arg[]){
         ExtendsThread extendsThread = new ExtendsThread(10);
